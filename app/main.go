f3bc3b4b153310ab27035a8e5479cf39d3f9f89b
sonarAIT <sonarmikumiku@gmail.com>
2023-03-08 05:53:26 +0900

    [feat] できた

--- app/main.go
+++ app/main.go
@@ -3,30 +3,71 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
-func RoutineReceive(ch chan string) {
-	for {
-		s, isActive := <-ch
-		if !isActive {
-			return
-		}
-		fmt.Println(s)
+type Elem struct {
+	sync.Mutex
+	Text             string
+	HeavyProcessTime time.Duration
+}
+
+func RoutineReceive(ReceiveC <-chan *Elem, HeavyProcessC chan<- *Elem, PrintC chan<- *Elem) {
+	for elem := range ReceiveC {
+		elem.Lock()
+		PrintC <- elem
+		HeavyProcessC <- elem
 	}
 }
 
-func RoutineHeavyProcess() {
+func RoutineHeavyProcess(HeavyProcessC <-chan *Elem) {
+	for elem := range HeavyProcessC {
+		// heavy process
+		time.Sleep(time.Second * elem.HeavyProcessTime)
 
+		elem.Unlock()
+	}
 }
 
-func RoutinePrint(wg *sync.WaitGroup) {
-
+func RoutinePrint(PrintC <-chan *Elem, wg *sync.WaitGroup) {
+	for elem := range PrintC {
+		elem.Lock()
+		fmt.Println(elem.Text)
+		wg.Done()
+	}
 }
 
 func main() {
-	ch := make(chan string)
-	go RoutineReceive(ch)
-	ch <- "hello"
-	ch <- "hello2"
-	ch <- "hello3"
+	// init
+	fmt.Println("init")
+	elems := []*Elem{
+		{
+			Text:             "hello",
+			HeavyProcessTime: 1,
+		},
+		{
+			Text:             "hello2",
+			HeavyProcessTime: 1,
+		},
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(len(elems))
+
+	ReceiveC := make(chan *Elem, 1024)
+	HeavyProcessC := make(chan *Elem, 1024)
+	PrintC := make(chan *Elem, 1024)
+	go RoutineReceive(ReceiveC, HeavyProcessC, PrintC)
+	for i := 0; i < 5; i++ {
+		go RoutineHeavyProcess(HeavyProcessC)
+	}
+	go RoutinePrint(PrintC, &wg)
+
+	// run
+	fmt.Println("run")
+	for _, elem := range elems {
+		ReceiveC <- elem
+	}
+
+	wg.Wait()
 }
